8156e5db195a1459eb85b3b321b070fb8d69c622
YuliyaKh <anelli6539@gmail.com>
2023-04-09 00:26:58 +0300

    изменен комментарий

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// если в поле маржа или наличие загружен мусор, то можно очистить их через консоль блаузера
-// только надо проверить, где в массиве начинаются мусорные значение
+// если в поле инфоблока загружен мусор, то можно очистить их через консоль блаузера
+// сначала надо проверить, где в массиве начинаются мусорные значение
 // тут в примере они начинаются с 5го элемента, поэтому i = 5
-// также надо првоерить селекторы, в других полях могут отличаться
+// также надо проверить селекторы, в других полях могут отличаться
 
 // очищает поле XML_ID
-let arr1 = window.document.querySelectorAll('[id^="PROPERTY_VALUES_XML_"]');
-for (let i = 5; i < arr1.length; i++) {
-	arr1[i].value = '';
+let arrXmlId = window.document.querySelectorAll('[id^="PROPERTY_VALUES_XML_"]');
+for (let i = 5; i < arrXmlId.length; i++) {
+	arrXmlId[i].value = '';
 }
 
 // очищает поле Значение
-let arr = window.document.querySelectorAll('[id^="PROPERTY_VALUES_VALUE_"]');
-for (let i = 5; i < arr.length; i++) {
-	arr[i].value = '';
+let arrVal = window.document.querySelectorAll('[id^="PROPERTY_VALUES_VALUE_"]');
+for (let i = 5; i < arrVal.length; i++) {
+	arrVal[i].value = '';
 }
